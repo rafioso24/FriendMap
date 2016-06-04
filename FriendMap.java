2770cfe396d88dc039646008f430acbf19e9c727
Rafael Moas <rafioso24@gmail.com>
2016-06-04 17:35:08 -0500

    Test

--- FriendMap.java
+++ FriendMap.java
@@ -1,6 +1,7 @@
-
-
-
-public static void main String(args[]){
-	system.out.println("Hello Github");
+public class FriendMap{
+	public static void main(String [ ] args){
+		System.out.println("Hello Github");
+	}	
 }
+
+
